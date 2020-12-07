b08976f5fffc2c86854bee428d34265ff9aaf29a
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-12-07 17:01:51 -0600

    api connected with axios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,34 @@ import React from 'react';
 import axios from 'axios';
 
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 class App extends React.Component {
-  state = {
-    
-  }
+	state = {
+		music: [],
+		loading: true
+	}
 
-  axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
+	componentDidMount() {
+		axios.get(`http://www.devcodecampmusiclibrary.com/api/music`)
+			.then(res => {
+				const music = res.data;
+				this.setState({ music });
+				this.setState({ loading: false });
+			})
+	}
+
+	render() {
+		if(this.state.loading == true){
+			return(<div>Loading...</div>)
+		}else{
+			return(
+				<p>
+					{this.state.music[0].title}
+				</p>
+			)
+		}
+	}
 }
 
 
